2167d9741d4ce57bb3b33dd1f2ca9031b174a872
GuilleMur <gmurval@etsinf.upv.es>
2024-10-17 20:31:01 +0200

    Añadida implementación de la clase ListArray3

--- ListArray.h
+++ ListArray.h
@@ -1,114 +1,107 @@
-#include <ostream>
 #include "List.h"
-template <typename T> 
-class ListArray {
-    private:
-    T* arr;
-	int max;
-	int n;
-	static const int MINSIZE = 2;
-	void resize(int new_size){
-		if (new_size < MINSIZE) {
-           		new_size = MINSIZE;
-       	}
-        // Crear un nuevo array dinámico de tamaño 'new_size'
-        T* new_arr = new T[new_size];
-        // Copiar los elementos actuales del array antiguo al nuevo
+#include <stdexcept>
+#include <iostream>
+#include <ostream>
+
+template <typename T>
+class ListArray : public List<T> {
+private:
+    T* array;
+    int max;
+    int n;
+    static const int MINSIZE = 2;
+
+    void resize(int new_size) {
+        T* new_array = new T[new_size];
         for (int i = 0; i < n; ++i) {
-    		new_arr[i] = arr[i];
+            new_array[i] = array[i];
         }
-        delete[] arr;
-        arr = new_arr;
+        delete[] array;
+        array = new_array;
         max = new_size;
-	}
-    public:
-    ListArray(){
-		max = MINSIZE;
-		n = 0;
-		arr = new T[max];	
-	}
+    }
 
-	~ListArray(){
-		delete[] arr;
-	}	
+public:
+    ListArray() : max(MINSIZE), n(0) {
+        array = new T[MINSIZE];
+    }
 
-	T& operator[](int pos){ // & para permitir modificaciones
-		if (pos < 0 || pos >= n) {
-           		 throw std::out_of_range("Posición fuera de rango");
-       		 }
-		return arr[pos];
-	}
-	friend std::ostream& operator<<(std::ostream &out, const ListArray<T> &list){
-		out << "[";
-       		for (int i = 0; i < list.n; ++i) {
-            		out << list.arr[i];
-            		if (i < list.n - 1) {
-                		out << ", "; // Separador entre elementos
-            		}
-        	}
-        	out << "]";
-        	return out;
-    	}
-	 void insert(int pos, T e) override{
-        // Verificar que la posición es válida
+    ~ListArray() {
+        delete[] array;
+    }
+
+    void insert(int pos, T element) override {
         if (pos < 0 || pos > n) {
-            throw std::out_of_range("Índice fuera de rango");
+            throw std::out_of_range("Index out of bounds");
         }
-
         if (n == max) {
-            resize(max * 2); 
+            resize(max * 2);
         }
-
         for (int i = n; i > pos; --i) {
-            arr[i] = arr[i - 1];
+            array[i] = array[i - 1];
         }
-        arr[pos] = e;
+        array[pos] = element;
         ++n;
     }
-    
-    void append(T e) override {
-        insert(n, e); 
-    }
-   
-    void prepend(T e) {
-        insert(0, e);  
+
+    void append(T element) override {
+        insert(n, element);
     }
 
-   
-     T remove(int pos) override{
+    void prepend(T element) override {
+        insert(0, element);
+    }
+
+    T remove(int pos) override {
         if (pos < 0 || pos >= n) {
-            throw std::out_of_range("Índice fuera de rango");
+            throw std::out_of_range("Index out of bounds");
         }
-        T removedElement = arr[pos];
-
+        T removed_element = array[pos];
         for (int i = pos; i < n - 1; ++i) {
-            arr[i] = arr[i + 1];
+            array[i] = array[i + 1];
         }
         --n;
-        return removedElement;
+        return removed_element;
     }
-    
+
     T get(int pos) override {
         if (pos < 0 || pos >= n) {
-            throw std::out_of_range("Posición inválida");
+            throw std::out_of_range("Index out of bounds");
         }
-        return arr[pos];
+        return array[pos];
     }
-   
-    int search(T e) override {
+
+    int search(T element) override {
         for (int i = 0; i < n; ++i) {
-            if (arr[i] == e) {
+            if (array[i] == element) {
                 return i;
             }
         }
-        return -1; 
+        return -1;
     }
+
+    int size() override {
+        return n;
+    }
+
     bool empty() override {
         return n == 0;
     }
-    
- 
-    int size() override {
-        return n;
+
+    T& operator[](int pos) {
+        if(pos < 0 || pos >= n){
+            throw std::out_of_range("Index out of bounds");
+        }
+        return array[pos];
+    }
+
+    friend std::ostream& operator<<(std::ostream& os, const ListArray<T>& list) {
+        for (int i = 0; i < list.n; ++i) {
+            os << list.array[i];
+            if (i < list.n - 1) {
+                os << ", ";
+            }
+        }
+        return os;
     }
 };
