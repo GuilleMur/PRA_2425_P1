238d829133251cbe4a16bf6139095b178caf5bdc
GuilleMur <gmurval@etsinf.upv.es>
2024-10-17 18:12:44 +0200

    Añadida implementación de la clase ListArray

--- ListArray.h
+++ ListArray.h
@@ -1,50 +1,44 @@
 #include <ostream>
 #include "List.h"
-
 template <typename T> 
 class ListArray : public List<T> {
-
     private:
-        T* arr;
+    T* arr;
 	int max;
 	int n;
 	static const int MINSIZE = 2;
 	void resize(int new_size){
 		if (new_size < MINSIZE) {
            		new_size = MINSIZE;
-       		}
-
+       	}
         // Crear un nuevo array dinámico de tamaño 'new_size'
-        	T* new_arr = new T[new_size];
-
+        T* new_arr = new T[new_size];
         // Copiar los elementos actuales del array antiguo al nuevo
-        	for (int i = 0; i < n; ++i) {
-            		new_arr[i] = arr[i];
-        	}
-        	delete[] arr;
-        	arr = new_arr;
-        	max = new_size;
+        for (int i = 0; i < n; ++i) {
+    		new_arr[i] = arr[i];
+        }
+        delete[] arr;
+        arr = new_arr;
+        max = new_size;
 	}
-
     public:
-        ListArray(){
+    ListArray(){
 		max = MINSIZE;
 		n = 0;
 		arr = new T[max];	
 	}
+
 	~ListArray(){
 		delete[] arr;
-
 	}	
-	T operator[](int pos){
+
+	T& operator[](int pos){ // & para permitir modificaciones
 		if (pos < 0 || pos >= n) {
            		 throw std::out_of_range("Posición fuera de rango");
        		 }
-
 		return arr[pos];
 	}
 	friend std::ostream& operator<<(std::ostream &out, const ListArray<T> &list){
-
 		out << "[";
        		for (int i = 0; i < list.n; ++i) {
             		out << list.arr[i];
@@ -55,42 +49,51 @@ class ListArray : public List<T> {
         	out << "]";
         	return out;
     	}
+	 void insert(int pos, T e) override{
+        // Verificar que la posición es válida
+        if (pos < 0 || pos > n) {
+            throw std::out_of_range("Índice fuera de rango");
+        }
 
-	void insert(int pos, T e) override {
-        	if (pos < 0 || pos > size()) {
-            		throw std::out_of_range("Posición inválida para inserción");
-        	}
-        	arr.insert(arr.begin() + pos, e);
-	 }
+        if (n == max) {
+            resize(max +1); 
+        }
 
+        for (int i = n; i > pos; --i) {
+            arr[i] = arr[i - 1];
+        }
+        arr[pos] = e;
+        ++n;
+    }
     
     void append(T e) override {
-        arr.push_back(e);
+        insert(n, e); 
+    }
+   
+    void prepend(T e) {
+        insert(0, e);  
     }
 
    
-    void prepend(T e) override {
-        arr.insert(data.begin(), e);
-    }
-
-   
-    T remove(int pos) override {
-        if (pos < 0 || pos >= size()) {
-            throw std::out_of_range("Posición inválida para eliminación");
+     T remove(int pos) override{
+        if (pos < 0 || pos >= n) {
+            throw std::out_of_range("Índice fuera de rango");
         }
         T removedElement = arr[pos];
-        arr.erase(arr.begin() + pos);
+
+        for (int i = pos; i < n - 1; ++i) {
+            arr[i] = arr[i + 1];
+        }
+        --n;
         return removedElement;
     }
-
     
     T get(int pos) override {
-        if (pos < 0 || pos >= size()) {
+        if (pos < 0 || pos >= size()-1) {
             throw std::out_of_range("Posición inválida");
         }
         return arr[pos];
     }
-
    
     int search(T e) override {
         for (int i = 0; i < size(); ++i) {
@@ -100,14 +103,16 @@ class ListArray : public List<T> {
         }
         return -1; 
     }
-
-
     bool empty() override {
-        return arr.empty();
+        if (n == 0) {
+            return true;
+        }else{
+            return false;
+        }
     }
     
  
     int size() override {
-        return arr.size();
+        return n;
     }
 };
